a863a1910a81a350dc44e21ae4943e96f9667207
Masdz <masdz1234567@gmail.com>
2018-06-04 16:32:09 -0500

    modificaciones en autobuses

--- Admin/controladores/autobuses/tabla.js
+++ Admin/controladores/autobuses/tabla.js
@@ -1,8 +1,9 @@
+var res;
 function actualizarAutobuses() {
     var datos = new XMLHttpRequest();
     datos.open("GET", "AUTOBUSES", false);
     datos.send(null);
-    var res = JSON.parse(datos.responseText);
+    res = JSON.parse(datos.responseText);
     var tabla = "";
     for (var i in res) {
         tabla += "<tr>";
@@ -10,14 +11,21 @@ function actualizarAutobuses() {
         tabla += "<td>" + res[i].placa + "</td>";
         tabla += "<td>" + res[i].origen + "-" + res[i].destino + "</td>";
         tabla += "<td>" + res[i].total + "</td>";
-        tabla += '<td><button class="btn btn-danger" data-toggle="modal" data-target="#confirmationModal" onclick="setidbusconductor('+res[i].Numeroautobus+')">'
+        tabla += '<td>'
+        tabla += '<button class="btn btn-danger" data-toggle="modal" data-target="#confirmationModal" onclick="setid('+res[i].Numeroautobus+')">'
         tabla += '<i class="fa fa-trash"></i>'
-        tabla += '</button></td>';
+        tabla += '</button>';
+        tabla += '   <button class="btn btn-primary" data-toggle="modal" data-target="#mmodificar" onclick="inimod('+i+')">'
+        tabla += '<i class="fa fa-edit"></i>'
+        tabla += '</button>';
+        tabla += '</td>';
         tabla += "</tr>";
     }
     document.getElementById("tabla").innerHTML = tabla;
+    initable1();
 }
-function iniselect(){
+
+function iniselect(select){
     var request=new XMLHttpRequest();
     request.open("GET","RUTAS",false);
     request.send(null);
@@ -28,7 +36,7 @@ function iniselect(){
         selec+=res[i].origen+"-"+res[i].destino;
         selec+="</option>";
     }
-    document.getElementById("selectruta").innerHTML=selec;
+    document.getElementById(select).innerHTML=selec;
 }
 function registrarAutobus(){
     var Numeroautobus=document.getElementById("ftnoautobus").value;
@@ -45,12 +53,15 @@ function registrarAutobus(){
     }
     request.send(JSON.stringify({Numeroautobus,placa,idruta,idlinea:1}));
 }
-function setidbusconductor(val){
+function setid(val){
     console.log("id:"+val);
     document.getElementById("idItem").innerHTML=val;
 }
+function getid(){
+    return document.getElementById("idItem").innerHTML;
+}
 function eliminar(){
-    var id=document.getElementById("idItem").innerHTML;
+    var id=getid();
     var request=new XMLHttpRequest();
     request.open("POST", "ELIMINARAUTOBUS", true);
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -62,5 +73,28 @@ function eliminar(){
     }
     request.send(JSON.stringify({id}));
 }
+function inimod(id){
+    console.log(res[id]);
+    iniselect("modruta");
+    document.getElementById("modplaca").value=res[id].placa;
+    document.getElementById("modruta").value=res[id].idruta;
+    setid(id);
+}
+function modificar(){
+    var id=getid();
+    var Numeroautobus=res[id].Numeroautobus;
+    var placa=document.getElementById("modplaca").value;
+    var idruta=document.getElementById("modruta").value;
+    var request=new XMLHttpRequest();
+    request.open("POST", "MODIFICARAUTOBUS", true);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.onreadystatechange = function() {
+        if(request.readyState == 4) {
+           alert(request.responseText);
+        }
+        actualizarAutobuses();
+    }
+    request.send(JSON.stringify({Numeroautobus,placa,idruta}));
+}
 actualizarAutobuses();
-iniselect();
+iniselect("selectruta");
